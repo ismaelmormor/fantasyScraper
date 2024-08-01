eaaa1680d479c9659812d0838ba5c9efd6a2f4a1
Ismael Moreno <ismael.moreno@geutebruck.es>
2024-08-01 15:00:20 +0200

    Initial commit

---
+++ probs.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled']
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.goto('https://www.futbolfantasy.com/jugadores/jan-oblak');
+    await page.waitForSelector('.pct');
+
+    const divContent = await page.$eval('.pct', div => div.innerText);
+    console.log(divContent);
+
+    await browser.close();
+})();
